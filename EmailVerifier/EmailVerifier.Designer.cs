758cb395ac15492fcfe1761c81254f8c8ba18a97
vovanduc <vovanduc1989@gmail.com>
2016-05-26 00:46:30 +0700

    Add EmailValidator dll

---
+++ EmailVerifier/EmailVerifier.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace EmailVerifier
+{
+    partial class EmailVerifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Location = new System.Drawing.Point(12, 12);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.Size = new System.Drawing.Size(516, 20);
+            this.txtEmailAddress.TabIndex = 0;
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Location = new System.Drawing.Point(555, 10);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(75, 23);
+            this.btnVerify.TabIndex = 1;
+            this.btnVerify.Text = "Verify";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 50);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(681, 373);
+            this.txtLog.TabIndex = 2;
+            this.txtLog.Text = "";
+            // 
+            // EmailVerifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 435);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.txtEmailAddress);
+            this.Name = "EmailVerifier";
+            this.Text = "Email Verifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmailAddress;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.RichTextBox txtLog;
+    }
+}
---
+++ EmailVerifier/EmailVerifier.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using Codicode;
+
+namespace EmailVerifier
+{
+    public partial class EmailVerifier : Form
+    {
+        public EmailVerifier()
+        {
+            InitializeComponent();
+
+            txtEmailAddress.Text = "dangquang10244@gmail.com";
+
+            btnVerify.Click += BtnVerify_Click;
+        }
+
+        private void BtnVerify_Click(object sender, EventArgs e)
+        {
+            //EmailValidator Ev = new EmailValidator();
+            //Ev.Mail_From = "dangquang1024@gmail.com";
+            //// Check if the email address is valid and really exists
+            //string errorMsg = Ev.Check_MailBox(txtEmailAddress.Text).ToString();
+
+            //if (errorMsg == "")
+            //{
+            //    // the Mail Address/Smtp is valid and running
+            //    txtLog.AppendText("\nMail OK");
+            //}
+            //else
+            //{
+            //    // An error occured while validating the email
+            //    txtLog.AppendText("\n" + errorMsg);
+            //}
+
+            //// Close and Dispose
+            //Ev.Dispose();
+            VerifyEmail(txtEmailAddress.Text);
+        }
+
+        public void VerifyEmail(string emailAdress)
+        {
+            TcpClient tClient = new TcpClient("gmail-smtp-in.l.google.com", 25);
+            string CRLF = "\r\n";
+            byte[] dataBuffer;
+            string ResponseString;
+
+            NetworkStream netStream = tClient.GetStream();
+            StreamReader reader = new StreamReader(netStream);
+            ResponseString = reader.ReadLine();
+            /* Perform HELO to SMTP Server and get Response */
+            dataBuffer = BytesFromString("HELO " + CRLF);
+            netStream.Write(dataBuffer, 0, dataBuffer.Length);
+            ResponseString = reader.ReadLine();
+            dataBuffer = BytesFromString("MAIL FROM:<dangquang1024@gmail.com>" + CRLF);
+            netStream.Write(dataBuffer, 0, dataBuffer.Length);
+            ResponseString = reader.ReadLine();
+            /* Read Response of the RCPT TO Message to know from google if it exist or not */
+
+            dataBuffer = BytesFromString("RCPT TO:<" + emailAdress + ">" + CRLF);
+            netStream.Write(dataBuffer, 0, dataBuffer.Length);
+            ResponseString = reader.ReadLine();
+            if (GetResponseCode(ResponseString) == 550)
+            {
+                txtLog.AppendText("Mai Address Does not Exist !");
+
+                txtLog.AppendText("Original Error from Smtp Server : " + ResponseString);
+            }
+            /* QUITE CONNECTION */
+            txtLog.AppendText("Email Id Existing !");
+            dataBuffer = BytesFromString("QUITE" + CRLF);
+            netStream.Write(dataBuffer, 0, dataBuffer.Length);
+            tClient.Close();
+        }
+
+        private byte[] BytesFromString(string str)
+        {
+            return Encoding.ASCII.GetBytes(str);
+        }
+        private int GetResponseCode(string ResponseString)
+        {
+            return int.Parse(ResponseString.Substring(0, 3));
+        }
+    }
+}
